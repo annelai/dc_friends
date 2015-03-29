6681123fe0b51c4e7740dfbe9bb28763109b762c
Anne <anne@tagtoo.org>
2015-03-29 17:00:25 +0800

    add 5 buffer

--- sync_controller.v
+++ sync_controller.v
@@ -69,8 +69,7 @@ module sync_controller (
 	wire 			rdclk;
 	wire	[9:0]	x;
 	wire	[9:0]	y;
-	reg		[1:0]	state;
-	reg		[1:0]	next_state;
+	reg		[1:0]	state, next_state;
 
 	reg		[4:0]	dvi_r;
 	reg		[5:0]	dvi_g;
@@ -85,11 +84,9 @@ module sync_controller (
 	reg		[5:0]	next_ccd_g;
 	reg		[4:0]	next_ccd_b;
 
-	reg 			rdreq;
-	reg				next_rdreq;
+	reg 			rdreq, next_rdreq;
 
-	reg 			start;
-	reg				next_start;
+	reg 			start, next_start;
 	reg		[9:0]	query_x;
 	reg		[9:0]	query_y;
 	reg		[9:0]	sync_x;
@@ -101,6 +98,10 @@ module sync_controller (
     
     reg             val, next_val;
 	reg				debug, next_debug;
+
+    reg     [35:0]  buffer1, buffer2, buffer3, buffer4, buffer5; // 10,10,5,6,5
+    reg     [35:0]  next_buffer1, next_buffer2, next_buffer3, next_buffer4, next_buffer5;
+    
 // ==== combinational part =================================
 	assign rdclk = clk_25;
 	assign x = next_query_x;
@@ -125,7 +126,13 @@ module sync_controller (
         next_val = 1'b0;
 		next_debug = 1'b0 || debug;
 
-		case(state)
+        next_buffer1 = 36'd0;
+        next_buffer2 = buffer1;
+        next_buffer3 = buffer2;
+        next_buffer4 = buffer3;
+        next_buffer5 = buffer4;
+		
+        case(state)
 			S_IDLE: begin
 				if(rdempty==1'b0) begin
 					next_state = S_WAIT;
@@ -139,20 +146,24 @@ module sync_controller (
 					next_dvi_r = q[23:19];
 					next_dvi_g = q[15:10];
 					next_dvi_b = q[7:3];
+                    next_buffer1 = {q[43:24], q[23:19], q[15:10], q[7:3]};
 					next_start = 1'b1;
                 end
 				if(ready==1'b1) begin
                     next_val = 1'b1;
-					next_sync_x = return_x;
-					next_sync_y = return_y;
-					next_ccd_r = r;
-					next_ccd_g = g;
-					next_ccd_b = b;
-                    next_rdreq = 1'b1;
-                    if(query_x!=return_x || query_y!=return_y) begin
+                    next_sync_x = buffer5[35:26];
+                    next_sync_y = buffer5[25:16];
+                    next_ccd_r = r;
+                    next_ccd_g = g;
+                    next_ccd_b = b;
+                    next_dvi_r = buffer5[15:11];
+                    next_dvi_g = buffer5[10:5];
+                    next_dvi_b = buffer5[4:0];
+                    if(buffer[35:26]!=return_x || buffer[25:16]!=return_y) begin
                         next_debug = 1'b1;
                     end
-                    if(rdempty==1'b1) begin
+                    next_rdreq = 1'b1;
+                    if(rdempty==1'b0) begin
                         next_state = S_IDLE;
                         next_rdreq = 1'b0;
                     end
@@ -160,7 +171,7 @@ module sync_controller (
 			end
 		endcase
 	end
-
+    
 // ==== sequential part ====================================
 	always@(posedge clk_25 or negedge rst_n) begin
 		if(rst_n==0) begin
@@ -179,6 +190,11 @@ module sync_controller (
 			sync_y 		<= 10'd0;
             debug       <= 1'b0;
             val         <= 1'b0;
+            buffer1     <= 36'd0;
+            buffer2     <= 36'd0;
+            buffer3     <= 36'd0;
+            buffer4     <= 36'd0;
+            buffer5     <= 36'd0;
 		end
 		else begin
 			state 		<= next_state;
@@ -196,6 +212,11 @@ module sync_controller (
 			sync_y 		<= next_sync_y;
             debug       <= next_debug;
             val         <= next_val;
+            buffer1     <= next_buffer1;
+            buffer2     <= next_buffer2;
+            buffer3     <= next_buffer3;
+            buffer4     <= next_buffer4;
+            buffer5     <= next_buffer5;
 		end
 	end
 endmodule
